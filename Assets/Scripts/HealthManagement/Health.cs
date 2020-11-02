da86b49b6100fb1b93834f1b56d029e636519b0f
Dutilleul <arnauddutilleul99@gmail.com>
2020-11-02 19:44:08 +0100

    health bar + health management

---
+++ Assets/Scripts/HealthManagement/Health.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace HealthManagement
+{
+    public class Health : MonoBehaviour, IDamageable
+    {
+        [SerializeField] private int currentHealth;
+        [SerializeField] private int maxHealth;
+        [SerializeField] private UnityEvent onDeath;
+
+        private void Start()
+        {
+            HealthManager.Instance.InitializationHealthBar();
+        }
+
+        // Start is called before the first frame update
+        public void TakeDamage(int damage)
+        {
+            currentHealth -= damage;
+            HealthManager.Instance.ModifyHealth(currentHealth);
+            if (currentHealth <= 0)
+            {
+                //Death Animation
+                //Block input of movement
+                //Wait for the end of animation
+                onDeath?.Invoke();
+            }
+        }
+    }
+}
