1595c38b41417308644d9a6005b8bf5693dd8bb2
Dutilleul <arnauddutilleul99@gmail.com>
2020-11-22 16:09:09 +0100

    Pickup lampe torche

---
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Inventory
+{
+    public class Inventory : MonoBehaviour
+    {
+        private IList<GameObject> _inventoryList;
+        public GameObject flashlight;
+
+        public IList<GameObject> InventoryList
+        {
+            get { return _inventoryList; }
+        }
+        void Start()
+        {
+            _inventoryList = new List<GameObject>(); 
+        }
+
+        public void TakeObject(GameObject obj)
+        {
+            InventoryList.Add(obj);
+
+            if (obj.name == "Flashlight")
+            {
+                flashlight.SetActive(true);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Inventory/Takeable.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Pooling;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Inventory
+{
+    public class Takeable : MonoBehaviour
+    {
+
+        private GameObject _takeMeGameObject;
+        public UnityEvent takeMeEvent;
+        public TMP_Text textTakeMe;
+
+        private bool _inside;
+
+        private void Start()
+        {
+            _takeMeGameObject = textTakeMe.transform.parent.gameObject;
+            _takeMeGameObject.SetActive(false);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            textTakeMe.text = "Take " + gameObject.name;
+            _takeMeGameObject.SetActive(true);
+            _inside = true;
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            _takeMeGameObject.SetActive(false);
+            textTakeMe.text = "";
+            _inside = false;
+        }
+
+        private void Update()
+        {
+            if (_inside && Input.GetKeyDown(KeyCode.E))
+            {
+                takeMeEvent?.Invoke();
+                _takeMeGameObject.SetActive(false);
+                gameObject.TryRelease();
+            }
+        }
+    }
+}
